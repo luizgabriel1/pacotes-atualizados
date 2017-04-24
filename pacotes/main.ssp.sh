b54f0bb075cb56f6c105f2764def895638517d58
TheLnunes15 <lucasnunes1995@hotmail.com>
2017-04-24 18:00:07 -0300

    Update main.ssp.sh

--- pacotes/main.ssp.sh
+++ pacotes/main.ssp.sh
@@ -68,7 +68,7 @@ sudo pacman -U *.tar.xz --needed --noconfirm
 cd ..
 echo "Repositorio configurado."
 
-pacman -Sy openldap ldap-client-config nss-pam-ldapd --needed --noconfirm
+pacman -Sy openldap ldap-client-config nss-pam-ldapd bluej --needed --noconfirm
 #sudo pacman -Syy --needed --noconfirm
 #sudo pacman -S ambiente-php ldap-client-config login-unico-dcomp sanity --needed --noconfirm
 
